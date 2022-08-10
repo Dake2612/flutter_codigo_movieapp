4b4335974197064ee51a3cfb6b03dd7b90c8a6f9
Dario Robles Roca <dake@MacBook-Pro-de-Dario-2.local>
2022-08-10 01:58:17 -0500

    http service and movie model

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_movieapp/models/movie_model.dart';
+import 'package:flutter_codigo_movieapp/ui/widgets/item_movie_widget.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
 
+  List<MovieModel> movies = [];
+
+  @override
+  initState() {
+    super.initState();
+    getMovies();
+  }
+
+  getMovies() async {
+    String _url =
+        "https://api.themoviedb.org/3/discover/movie?api_key=9fc7c82372c3811e921c8579fe36207c&language=en-US&page=1";
+    Uri _uri = Uri.parse(_url);
+    http.Response _response = await http.get(_uri);
+    if(_response.statusCode == 200){
+      Map<String,dynamic> moviesMap = json.decode(_response.body);
+      movies = moviesMap["results"].map<MovieModel>((e) => MovieModel.fromJson(e)).toList();
+      print(movies);
+      setState((){});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Color(0xff23232D),
       body: SafeArea(
@@ -57,136 +83,15 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 20.0,
                 ),
-                Container(
-                  height: height * 0.65,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.white12,
-                      borderRadius: BorderRadius.circular(16.0),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          'https://cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/S526ZQGBDNBFXOFZXJSHOXHDVU.jpg',
-                        ),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 5),
-                          blurRadius: 12.0,
-                        )
-                      ]),
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 12.0, vertical: 15.0),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Color(0xff23232D).withOpacity(0.92),
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Lorem ipsum dolor sit amet',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              const SizedBox(
-                                height: 6.0,
-                              ),
-                              Container(
-                                width: 100,
-                                height: 3.2,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff09C677),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 6.0,
-                              ),
-                              Text(
-                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Colors.white.withOpacity(0.85),
-                                    fontSize: 14.0),
-                              ),
-                              const SizedBox(
-                                height: 6.0,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.date_range,
-                                        color: Colors.white,
-                                        size: 18.0,
-                                      ),
-                                      SizedBox(
-                                        width: 6.0,
-                                      ),
-                                      Text(
-                                        '2022-02-12',
-                                        style: TextStyle(color: Colors.white),
-                                      )
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.thumb_up,
-                                        color: Colors.white,
-                                        size: 18.0,
-                                      ),
-                                      SizedBox(
-                                        width: 6.0,
-                                      ),
-                                      Text(
-                                        '433',
-                                        style: TextStyle(color: Colors.white),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                          padding: const EdgeInsets.all(16.0),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color(0xff23232D).withOpacity(0.92),
-                          ),
-                          child: Text(
-                            '4.2',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: ScrollPhysics(),
+                  itemCount: movies.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return ItemMovieWidget(
+                      movieModel: movies[index],
+                    );
+                  },
                 ),
               ],
             ),
